b298c98e6d344d611a9977f6e766603c78d0db6b
joao curvelo <joao.pedro1905@gmail.com>
2024-08-12 17:41:14 -0300

    feat: containerized application with dockerfile and docker compose

--- monitor/agent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-BASE_DIR="$(pwd)"
-
-python3.9 $BASE_DIR/workers/agent.py
--- monitor/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-BASE_DIR="$(pwd)"
-
-export METRICS_PATH="${BASE_DIR}/metrics.txt"
-export X_API_KEY="defaultkey" 
