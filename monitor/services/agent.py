46175276bc7b4fe73587958d9aba4b6c8cd85bb3
Joao1905 <joao.pedro1905@gmail.com>
2024-09-12 19:27:30 -0300

    feat: execute monitor in scone hw mode

--- monitor/services/agent.py
+++ monitor/services/agent.py
@@ -1,8 +1,8 @@
-import threading
 import psutil
 import os
 import sys
-import uuid
+import time
+import threading
 
 executing_dir, _ = os.path.split(os.path.abspath(__file__))
 sys.path.append(os.path.join(executing_dir, '..'))
@@ -11,15 +11,12 @@ from models.metric import Metric
 from errors.errors import InsufficientDelay
 
 class Agent:
-    __is_running = False
-
-    def __init__(self, agent_id, monitor_delay_secs = 10, collect_interval_secs = 3, metrics_file_path = False):
-        self.__cpu_collect_interval = int(collect_interval_secs)
+    def __init__(self, agent_id, monitor_delay_secs = 10, metrics_file_path = False):
         self.__monitor_delay_secs = int(monitor_delay_secs)
 
-        insufficient_time_delay = self.__cpu_collect_interval * 3 > self.__monitor_delay_secs
+        insufficient_time_delay = self.__monitor_delay_secs < 1
         if (insufficient_time_delay):
-            raise InsufficientDelay('collect interval must be at least 3 times lower than monitor delay')
+            raise InsufficientDelay('collect interval must be of at least 1 second')
 
         self.__metrics_file_path = metrics_file_path
         if not metrics_file_path:
@@ -32,24 +29,32 @@ class Agent:
     def get_metrics_file_path(self):
         return self.__persist_path
 
+
     def start(self):
-        if not self.__is_running:
-            self.__monitor_thread = threading.Timer(self.__monitor_delay_secs, self.__collect_and_persist_data)
-            self.__monitor_thread.start()
-            self.__is_running = True
-    
-    def stop(self):
-        self.__monitor_thread.cancel()
-        self.__is_running = False
+        psutil.cpu_percent(interval=None)   # required
+        while True:
+            time.sleep(self.__monitor_delay_secs)
+            self.__collect_and_persist_data()
 
 
     def __collect_and_persist_data(self):
-        self.__is_running = False
-        self.start()
-
-        cpu_usage_percent = psutil.cpu_percent(self.__cpu_collect_interval)
-        mem_usage_gb = psutil.virtual_memory()[3]/1000000000
+        cpu_usage_percent = psutil.cpu_percent(interval=None)
+        mem_usage_gb = self.__get_memory_info()
         metric = Metric(self.__agent_id, cpu_usage_percent, mem_usage_gb)
-
         with open(self.__metrics_file_path, "a+") as metrics_file:
             metrics_file.write(metric.to_output() + '\n')
+
+
+    def __get_memory_info(self):
+        meminfo = {}
+        
+        with open('/host/proc/meminfo', 'r') as f:
+            for line in f:
+                parts = line.split()
+                key = parts[0].strip(':')
+                value = int(parts[1])
+                meminfo[key] = value
+
+        total_memory = meminfo['MemTotal'] / 1024**2
+        available_memory = meminfo['MemAvailable'] / 1024**2
+        return total_memory - available_memory
--- monitor/workers/agent.py
+++ monitor/workers/agent.py
@@ -18,11 +18,10 @@ def main():
         os.remove(metrics_path)
     
     monitor_delay_secs = os.getenv('MONITOR_DELAY_SECS')
-    collect_interval_secs = os.getenv('COLLECT_INTERVAL_SECS')
 
     agent = Agent(agent_id, metrics_file_path=metrics_path)
-    if monitor_delay_secs and collect_interval_secs:
-        agent = Agent(agent_id, monitor_delay_secs, collect_interval_secs, metrics_file_path=metrics_path)
+    if monitor_delay_secs:
+        agent = Agent(agent_id, monitor_delay_secs, metrics_file_path=metrics_path)
     
     agent.start()
 
